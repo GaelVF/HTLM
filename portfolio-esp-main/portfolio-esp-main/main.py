b1e29118fba9d499b6ee8f4c5908c8be030d26a9
GaelVF <gaelvargasf593@gmail.com>
2024-11-21 19:32:02 -0500

    Add files via upload

---
+++ portfolio-esp-main/portfolio-esp-main/main.py
@@ -0,0 +1,33 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# Página de contenidos en ejecución
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Habilidades dinámicas
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+@app.route('/', methods=['POST'])
+def process_form():
+    button_discord = request.form.get('button_discord')
+    return render_template('index.html', button_discord=button_discord)
+
+
+@app.route('/feedback', methods=['POST'])
+def process_feedback():
+    email = request.form['email']
+    text = request.form['text']
+    return render_template('feedback.html', email=email,text=text)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
